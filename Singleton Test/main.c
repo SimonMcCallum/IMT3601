026464eafe3548ea58ce22e36f505dab11221df3
Simon McCallum <simon.mccallum@hig.no>
2012-08-31 19:29:02 +0200

    Adding unit test

---
+++ Singleton Test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Singleton Test
+//
+//  Created by Simon McCallum on 31/08/2012.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
