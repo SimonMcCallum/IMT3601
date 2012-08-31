e10279f41bc4008117ec04d6337181ac633c5ad6
Simon McCallum <simon.mccallum@hig.no>
2012-08-31 08:13:04 +0200

    Adding Singleton Pattern files.  Untested but content should be fine. Needs Testing

---
+++ Singleton.cpp
@@ -0,0 +1,9 @@
+//
+//  Singleton.cpp
+//  IMT3601
+//
+//  Created by Simon McCallum on 30/08/2012.
+//
+//
+
+#include "Singleton.h"
---
+++ Singleton.h
@@ -0,0 +1,14 @@
+//
+//  Singleton.h
+//  IMT3601
+//
+//  Created by Simon McCallum on 30/08/2012.
+//
+//
+
+#ifndef __IMT3601__Singleton__
+#define __IMT3601__Singleton__
+
+#include <iostream>
+
+#endif /* defined(__IMT3601__Singleton__) */
